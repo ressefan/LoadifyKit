63e66a221999161e2272ec6858ffa0b6cf1c42c7
VishwaiOSDev <vishwa300499@icloud.com>
2022-10-29 20:13:17 +0530

    Migrated assets and colors

---
+++ Sources/LoadifyKit/ColorKit/ColorKit.swift
@@ -0,0 +1,22 @@
+//
+//  ColorKit.swift
+//  
+//
+//  Created by Vishweshwaran on 29/10/22.
+//
+
+import SwiftUI
+
+extension LoadifyKit {
+    
+    public struct ColorKit {
+        public static let appBackground = Color("app_background", bundle: .module)
+        public static let greyText = Color("grey_text", bundle: .module)
+        public static let textfieldBackground = Color("textfield_background", bundle: .module)
+        public static let blueAccent = Color("blue_accent", bundle: .module)
+        public static let errorRedGradient = Color("error_red_gradient", bundle: .module)
+        public static let errorRed = Color("error_red", bundle: .module)
+        public static let successGreenGradient = Color("successG_green_gradient", bundle: .module)
+        public static let successGreen = Color("success_green", bundle: .module)
+    }
+}
